8f4b5fca3969bbdcc4ec4d5aabb1c896483ae788
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-05 16:33:46 +0300

    Add tests to user-reducer.test.ts

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -12,4 +12,8 @@ test('user reducer should increment only age', () => {
 test('user reducer should increment only childrenCount', () => {
     const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
     // your code here
+
+    const endState = userReducer(startState, {type: 'INCREMENT-CHILDREN-COUNT'})
+
+    expect(endState.childrenCount).toBe(3)
 })
