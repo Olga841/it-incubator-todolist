fb5544a0e2d15b5339077b8ef31225b323a6c6c7
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-05 22:01:44 +0300

    Add case 'CHANGE-TODOLIST-FILTER' to tests

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -57,6 +57,30 @@ test('correct todolist should change its name', () => {
     expect(endState[1].title).toBe(newTodolistTitle)
 })
 
+​test('correct filter of todolist should be changed', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newFilter: FilterValuesType = 'completed'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'CHANGE-TODOLIST-FILTER',
+        id: todolistId2,
+        filter: newFilter
+    }
+
+    const endState = todolistsReducer(startState, action)
+
+    expect(endState[0].filter).toBe('all')
+    expect(endState[1].filter).toBe(newFilter)
+})
+
+
 
 
 
